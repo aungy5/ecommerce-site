67393f2c259e3fe7ea18734b2d5a9e18693e53c8
Andrew Ungvary <aungvary5@gmail.com>
2021-11-04 17:29:51 -0400

    adding seeders

---
+++ server/seeders/seed.js
@@ -0,0 +1,37 @@
+const faker = require('faker')
+const db = require('../config/connection')
+const { User, Comment, Post } = require('../models')
+
+db.once('open', async () => {
+    await User.deleteMany({});
+    await Comment.deleteMany({});
+
+    const userData = [];
+
+    for (let i = 0; i < 25; i++) {
+        const username = faker.internet.userName();
+        const email = faker.internet.email();
+        const password = faker.internet.password();
+
+        userData.push({ username, email, password })
+    }
+
+    const createdUsers = await User.collection.insert(userData);
+
+    let commentData = [];
+    for (let i = 0; i < 25; i++) {
+        const commentBody = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+        const randomUser = Math.floor(Math.random() * createdUsers.ops.length);
+        const { username, _id: userId } = createdUsers.ops[randomUser];
+        const madeComment = await Comment.create({ commentBody, username });
+
+        const updatedUser = await User.updateOne(
+            { _id: userId },
+            { $push: { comments: madeComment._id } }
+        );
+        commentData.push(madeComment);
+    }
+
+    console.log('DB SEEDED SUCCESSFULLY :)');
+    process.exit(0);
+})
